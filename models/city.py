118ccd90b64ee8e0fd9e68ce4df06c363803da8e
Jesus <jesus.junc10@gmail.com>
2022-09-08 23:10:50 -0500

    8 task

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object):
@@ -11,6 +12,7 @@ class City(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object)
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
     else:
         state_id = ""
         name = ""
