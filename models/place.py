ccaa45373bb05097dbbf4ae278ddf8a8ee929771
Jesus <jesus.junc10@gmail.com>
2022-09-14 19:38:12 -0500

    10 task many to many

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from os import getenv
 from sqlalchemy.orm import relationship
 
@@ -21,6 +21,15 @@ class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         review = relationship("Review", backref="place", cascade="delete")
+        place_amenity = Table('place_amenity', Base.metadata, Column(
+            'place_id', String(60), ForeignKey(
+                'places.id'), primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey(
+                'amenities.id'), primary_key=True,
+            nullable=False)
+        )
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -33,6 +42,7 @@ class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
         @property
         def review(self):
             """return the list of Review instance """
@@ -42,3 +52,15 @@ class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object
                 if value.__class__ == 'Review' and self.id == value.place_id:
                     my_list.append(value)
             return my_list
+
+        @property
+        def amenities(self):
+            """return the list of Amenity"""
+            if len(self.amenity_ids) > 0:
+                return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, value):
+            """handles append method for adding an amenity id"""
+            if value.__class__.__name__ == 'Amenity':
+                self.amenity_ids.append(value)
